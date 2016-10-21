e130d5901839139eaf36f1a21c6d734b188db2d4
Britton Baumann <brittonbaumann@gmail.com>
2016-10-21 08:34:30 -0500

    Added a index about and contact page with gems and normalize

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'about' => 'welcome#about'
+
+  get 'contact' => 'welcome#contact'
+
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
